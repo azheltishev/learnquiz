46ac4927ade2525b01aa518dce66b475e3f85739
Aleksandr Zheltishev <azheltishevs@gmail.com>
2021-04-11 11:21:09 +0300

    random words asking + score counter

--- src/main/java/lv/progmeistars/Main.java
+++ src/main/java/lv/progmeistars/Main.java
@@ -4,18 +4,40 @@ import lv.progmeistars.parser.CsvParser;
 import lv.progmeistars.parser.WordTranslationTypeData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
         var parser = new CsvParser();
+        Random random = new Random();
 
         var file = Main.class.getClassLoader().getResource("words.csv").getFile();
         var list = parser.parseFile(new File(file));
 
-        for (WordTranslationTypeData data : list) {
-            System.out.println(data);
+        int size = list.size();
+        var askedWords = new ArrayList<WordTranslationTypeData>();
+        for (int i = 0; i < 5; i++) {
+            askedWords.add(list.get(random.nextInt(size)));
         }
+
+
+        int score = 0;
+        Scanner scanner = new Scanner(System.in);
+        for (WordTranslationTypeData data : askedWords) {
+            System.out.println(data.getSourceLanguage());
+            String next = scanner.next();
+            if (data.getTranslationLanguage().contains(next.strip())) {
+                System.out.println("correct!");
+                score++;
+            } else {
+                System.out.printf("wrong translation, correct would be : %s !%n", data.getTranslationLanguage());
+            }
+        }
+
+        System.out.println("you scored " + score + " out of 5");
     }
 }
