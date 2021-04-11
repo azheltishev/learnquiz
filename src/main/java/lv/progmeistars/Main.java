631cc8ca3a52f673abd919666d6e0d322c533175
Aleksandr Zheltishev <azheltishevs@gmail.com>
2021-04-11 11:12:30 +0300

    Print out parsed translated words

--- src/main/java/lv/progmeistars/Main.java
+++ src/main/java/lv/progmeistars/Main.java
@@ -2,20 +2,24 @@ package lv.progmeistars;
 
 import lv.progmeistars.parser.CsvParser;
 import lv.progmeistars.parser.WordTranslationTypeData;
+import lv.progmeistars.word.WordParser;
 
 import java.io.File;
+import java.util.List;
 
 public class Main {
 
 
     public static void main(String[] args) {
+        var wordParser = new WordParser();
         var parser = new CsvParser();
 
         var file = Main.class.getClassLoader().getResource("words.csv").getFile();
         var list = parser.parseFile(new File(file));
 
         for (WordTranslationTypeData data : list) {
-            System.out.println(data);
+            List<String> parsedTranslations = wordParser.parse(data.getTranslationLanguage());
+            System.out.printf("%s | %s%n", data.getSourceLanguage(), String.join(",", parsedTranslations));
         }
     }
 }
